d9383e474f47882309b062ab9bdc1c8f363c2c8d
luis <gzanabriaz28@gmail.com>
2023-04-04 21:52:31 -0500

    proveedor controller

--- src/main/java/com/example/lab2_gtics/controllers/ProveedorController.java
+++ src/main/java/com/example/lab2_gtics/controllers/ProveedorController.java
@@ -1,4 +1,33 @@
 package com.example.lab2_gtics.controllers;
 
+
+import com.example.lab2_gtics.entity.Proveedores;
+import com.example.lab2_gtics.repository.ProveedoresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class ProveedorController {
+
+    final ProveedoresRepository proveedoresRepository;
+    public ProveedorController (ProveedoresRepository proveedoresRepository) {
+        this.proveedoresRepository = proveedoresRepository;
+    }
+
+    @GetMapping("/proveedores/lista")
+    public String mostrarListaProveedores(Model model) {
+        List<Proveedores> listaProveedores = proveedoresRepository.findAll();
+        model.addAttribute("listaProveedores",listaProveedores);
+        return "proveedores/lista";
+    }
+
+
+
+
+
 }
--- src/main/java/com/example/lab2_gtics/entity/Proveedores.java
+++ src/main/java/com/example/lab2_gtics/entity/Proveedores.java
@@ -5,10 +5,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Getter
+@Setter
+@Entity
 @Table(name ="proveedores")
 public class Proveedores {
-
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
